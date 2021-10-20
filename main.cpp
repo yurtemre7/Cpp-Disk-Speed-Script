8e308795baa309e0a8402b1c4805473ebac2a9d6
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2021-10-20 14:05:17 +0200

    better thing

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@
 #include <algorithm>
 #include <fstream>
 
-
 int main()
 {
     csr::util::Timer<csr::args::TTS::ms> timer;
 
+    std::ifstream src("bigobject.txt", std::ios::binary);
+    std::ofstream dst("move_here/here.txt", std::ios::binary);
+
     timer.resume();
-    
-    std::ifstream  src("bigobject.txt", std::ios::binary);
-    std::ofstream  dst("move_here/here.txt",   std::ios::binary);
 
     dst << src.rdbuf();
 
     timer.pause();
 
+    std::cout << "Time taken to move the file " << timer.getTime() << " ms" << std::endl;
+
     dst.close();
     src.close();
 
-    std::cout << "Time taken to move the file " << timer.getTime() << " ms" << std::endl;
     return 0;
 }
